05791eb31f828994c351f89cab3b79d15ceacffc
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2021-12-07 22:59:25 -0300

    Day 1 part 1

---
+++ Day1/Program.cs
@@ -0,0 +1,10 @@
+﻿var parsedDepths = File.ReadAllLines("input.txt");
+var depths = parsedDepths.Select(p => int.Parse(p)).ToList();
+
+var increasedMeasurements = 0;
+for (int currentDepth = 1; currentDepth < depths.Count; currentDepth++)
+{
+    if (depths[currentDepth] > depths[currentDepth - 1]) { increasedMeasurements++; }
+}
+
+Console.WriteLine(increasedMeasurements);
