3c25bb0906091a69138fbdbffc5435a347580f9c
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2021-12-08 00:54:48 -0300

    Day 6 part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -2,25 +2,42 @@
 var parsedTimers = input.Split(',');
 var timers = parsedTimers.Select(t => int.Parse(t)).ToList();
 
-var dayCount = 80;
-var timersCountAtStartOfDay = timers.Count;
+var dayCount = 256;
+var timerValueToCountOnValue = new Dictionary<int, long>();
+
+foreach (var timer in timers)
+{
+    if (!timerValueToCountOnValue.ContainsKey(timer))
+    {
+        timerValueToCountOnValue.Add(timer, 1);
+    }
+    else
+    {
+        timerValueToCountOnValue[timer]++;
+    }
+}
+
+for (int i = 0; i <= 8; i++)
+{
+    if (!timerValueToCountOnValue.ContainsKey(i))
+    {
+        timerValueToCountOnValue.Add(i, 0);
+    }
+}
 
 for (var i = 0; i < dayCount; i++)
 {
-    for (var j = 0; j < timersCountAtStartOfDay; j++)
+    var timersOnNextValue = 0L;
+    var timersOnCurrentValue = timerValueToCountOnValue[8];
+    for (var j = 8; j > 0; j--)
     {
-        var timer = timers[j];
-        if (timer == 0)
-        {
-            timers[j] = 6;
-            timers.Add(8);
-        }
-        else
-        {
-            timers[j] -= 1;
-        }
+        timersOnNextValue = timerValueToCountOnValue[j - 1];
+        timerValueToCountOnValue[j - 1] = timersOnCurrentValue;
+        timersOnCurrentValue = timersOnNextValue;
     }
-    timersCountAtStartOfDay = timers.Count;
+
+    timerValueToCountOnValue[6] += timersOnNextValue;
+    timerValueToCountOnValue[8] = timersOnNextValue;
 }
 
-Console.WriteLine(timersCountAtStartOfDay);
+Console.WriteLine(timerValueToCountOnValue.Values.Sum());
