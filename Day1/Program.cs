3125efe15fd815b5e82a44e4ddbedadffd393eaf
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2021-12-07 23:05:43 -0300

    Day 1 part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -2,9 +2,11 @@
 var depths = parsedDepths.Select(p => int.Parse(p)).ToList();
 
 var increasedMeasurements = 0;
-for (int currentDepth = 1; currentDepth < depths.Count; currentDepth++)
+for (int currentDepth = 3; currentDepth < depths.Count; currentDepth++)
 {
-    if (depths[currentDepth] > depths[currentDepth - 1]) { increasedMeasurements++; }
+    var previousWindowMeasurement = depths[currentDepth - 1] + depths[currentDepth - 2] + depths[currentDepth - 3];
+    var currentWindowMeasurement = depths[currentDepth] + depths[currentDepth - 1] + depths[currentDepth - 2];
+    if (currentWindowMeasurement > previousWindowMeasurement) { increasedMeasurements++; }
 }
 
 Console.WriteLine(increasedMeasurements);
