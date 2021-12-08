7dd860b9b7ecb1da9e75d57d427bb620a404d597
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2021-12-07 22:21:29 -0300

    Day 7 part 1

---
+++ Day7/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+using var parser = new TextFieldParser("input.txt") { Delimiters = new[] { "," } };
+var parsedPositions = parser.ReadFields()!;
+var positions = parsedPositions.Select(p => int.Parse(p)).ToList();
+
+var minPosition = positions.Min();
+var maxPosition = positions.Max();
+var positionToFuel = new Dictionary<int, int>();
+
+for (var currentPosition = minPosition; currentPosition <= maxPosition; currentPosition++)
+{
+    var fuel = 0;
+    foreach (var position in positions)
+    {
+        fuel += Math.Abs(position - currentPosition);
+    }
+    positionToFuel.Add(currentPosition, fuel);
+}
+
+foreach (var pair in positionToFuel.OrderByDescending(p => p.Value))
+{
+    Console.WriteLine(pair);
+}
