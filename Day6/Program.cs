7b84bbd2d1fc27392ddc68b274fc2d1046b67ced
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2021-12-07 23:53:07 -0300

    Day 6 part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllText("input.txt");
+var parsedTimers = input.Split(',');
+var timers = parsedTimers.Select(t => int.Parse(t)).ToList();
+
+var dayCount = 80;
+var timersCountAtStartOfDay = timers.Count;
+
+for (var i = 0; i < dayCount; i++)
+{
+    for (var j = 0; j < timersCountAtStartOfDay; j++)
+    {
+        var timer = timers[j];
+        if (timer == 0)
+        {
+            timers[j] = 6;
+            timers.Add(8);
+        }
+        else
+        {
+            timers[j] -= 1;
+        }
+    }
+    timersCountAtStartOfDay = timers.Count;
+}
+
+Console.WriteLine(timersCountAtStartOfDay);
