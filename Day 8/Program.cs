227df2eaff0f088f66d50fe021df5c8b7f1a6b3d
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2021-12-08 02:47:22 -0300

    Day 8 part 1

---
+++ Day 8/Program.cs
@@ -0,0 +1,14 @@
+﻿var entries = File.ReadAllLines("input.txt");
+entries = entries.Select(e => e[(e.IndexOf("|") + 2)..]).ToArray();
+var digits = entries.SelectMany(e => e.Split(' '));
+
+var count = 0;
+foreach (var digit in digits)
+{
+    if (digit.Length == 2 || digit.Length == 3 || digit.Length == 4 || digit.Length == 7)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
