39a2afe28b210a950fe959988ae1526da5b1e594
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2021-12-10 01:27:50 -0300

    Day 9 part 2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,9 +1,11 @@
-﻿var lines = File.ReadAllLines("input.txt");
+﻿using System.Drawing;
+
+var lines = File.ReadAllLines("input.txt");
 var width = lines[0].Length;
 var height = lines.Length;
 
 var map = new int[width, height];
-var lowPoints = new List<(int x, int y)>();
+var lowPoints = new List<Point>();
 
 for (var y = 0; y < lines.Length; y++)
 {
@@ -23,15 +25,85 @@ for (var y = 0; y < height; y++)
             && (y <= 0 || map[x, y] < map[x, y - 1])
             && (y >= height - 1 || map[x, y] < map[x, y + 1]))
         {
-            lowPoints.Add((x, y));
+            lowPoints.Add(new Point(x, y));
         }
     }
 }
 
-var riskSum = 0;
-foreach (var (x, y) in lowPoints)
+List<Point> GetBasinHigherNeighbors(Point point)
 {
-    riskSum += map[x, y] + 1;
+    var basinHigherNeighbors = new List<Point>();
+    if (point.X > 0 && map![point.X, point.Y] < map[point.X - 1, point.Y] && map[point.X - 1, point.Y] < 9)
+    {
+        var neighbor = new Point(point.X - 1, point.Y);
+        basinHigherNeighbors.Add(neighbor);
+        basinHigherNeighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+    if (point.X < width - 1 && map![point.X, point.Y] < map[point.X + 1, point.Y] && map[point.X + 1, point.Y] < 9)
+    {
+        var neighbor = new Point(point.X + 1, point.Y);
+        basinHigherNeighbors.Add(neighbor);
+        basinHigherNeighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+    if (point.Y > 0 && map![point.X, point.Y] < map[point.X, point.Y - 1] && map[point.X, point.Y - 1] < 9)
+    {
+        var neighbor = new Point(point.X, point.Y - 1);
+        basinHigherNeighbors.Add(neighbor);
+        basinHigherNeighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+    if (point.Y < height - 1 && map![point.X, point.Y] < map[point.X, point.Y + 1] && map[point.X, point.Y + 1] < 9)
+    {
+        var neighbor = new Point(point.X, point.Y + 1);
+        basinHigherNeighbors.Add(neighbor);
+        basinHigherNeighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+
+    return basinHigherNeighbors;
 }
 
-Console.WriteLine(riskSum);
+HashSet<Point> FindBasin(Point lowPoint)
+{
+    var neighbors = new List<Point>();
+    if (lowPoint.X > 0 && map[lowPoint.X - 1, lowPoint.Y] < 9)
+    {
+        var neighbor = new Point(lowPoint.X - 1, lowPoint.Y);
+        neighbors.Add(neighbor);
+        neighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+    if (lowPoint.X < width - 1 && map[lowPoint.X + 1, lowPoint.Y] < 9)
+    {
+        var neighbor = new Point(lowPoint.X + 1, lowPoint.Y);
+        neighbors.Add(neighbor);
+        neighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+    if (lowPoint.Y > 0 && map[lowPoint.X, lowPoint.Y - 1] < 9)
+    {
+        var neighbor = new Point(lowPoint.X, lowPoint.Y - 1);
+        neighbors.Add(neighbor);
+        neighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+    if (lowPoint.Y < height - 1 && map[lowPoint.X, lowPoint.Y + 1] < 9)
+    {
+        var neighbor = new Point(lowPoint.X, lowPoint.Y + 1);
+        neighbors.Add(neighbor);
+        neighbors.AddRange(GetBasinHigherNeighbors(neighbor));
+    }
+
+    var basinPoints = new HashSet<Point> { lowPoint };
+    foreach (var neighbor in neighbors)
+    {
+        basinPoints.Add(neighbor);
+    }
+
+    return basinPoints;
+}
+
+var basins = new HashSet<Point>[lowPoints.Count];
+for (int i = 0; i < basins.Length; i++)
+{
+    basins[i] = FindBasin(lowPoints[i]);
+}
+
+var threeBiggestBasins = basins.OrderByDescending(basin => basin.Count).Take(3).ToList();
+
+Console.WriteLine(threeBiggestBasins[0].Count * threeBiggestBasins[1].Count * threeBiggestBasins[2].Count);
